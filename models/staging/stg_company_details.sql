9f94a56855d48cce60cd9c2afb2a6326af308784
Advika-Sharma <advika.sharma@atrium.ai>
2026-02-13 09:30:21 +0000

    New branch demo_apurva created from main

--- models/staging/stg_company_details.sql
+++ models/staging/stg_company_details.sql
@@ -1,12 +1,12 @@
 {{ config(materialized='view') }}
 
 SELECT
-    Symbol AS ticker,
-    Sector,
-    Industry,
-    Marketcap,
-    Currentprice,
-    Ebitda,
-    Revenuegrowth,
-    Country
+    "Symbol" AS ticker,
+    "Sector" AS sector,
+    "Industry" AS industry,
+    "Marketcap" AS market_cap,
+    "Currentprice" AS current_price,
+    "Ebitda" AS ebitda,
+    "Revenuegrowth" AS revenue_growth,
+    "Country" AS country
 FROM {{ source('raw', 'companies_details_raw') }}
